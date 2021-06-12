7ef0af465d6e95c20d95e5e5dd49c296686294ed
felipe <felipe_v.s@hotmail.com>
2021-06-11 21:18:30 -0300

    39 - The concept of composition

--- starting-setup/src/components/Expenses.js
+++ starting-setup/src/components/Expenses.js
@@ -1,16 +1,17 @@
+import Card from './Card';
 import ExpenseItem from './ExpenseItem';
 
 import './Expenses.css';
 
 function Expenses(props) {
     return(
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem
                 title={props.expenses[0].title}
                 amount={props.expenses[0].amount}
                 date={props.expenses[0].date}
             />
-        </div>
+        </Card>
     )
 }
 
